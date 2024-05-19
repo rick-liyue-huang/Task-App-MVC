d64a1552d670048819430d0fa4ce7b56ff9db4f9
Rick Huang <rick.liyue.huang@gmail.com>
2024-05-19 19:34:14 +1000

    ci: create the new project

---
+++ Task-App-MVC-Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Task_App_MVC_Web.Models;
+
+namespace Task_App_MVC_Web.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ Task-App-MVC-Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Task_App_MVC_Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
